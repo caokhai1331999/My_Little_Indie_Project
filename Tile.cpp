3c1c7df88a217ac30be788c07b5d670f99de2757
Cao Khai <goku1331999@gmail.com>
2025-10-30 21:07:33 +0700

    latest update

---
+++ Tile.cpp
@@ -0,0 +1,25 @@
+/* ========================================================================
+   $File: $
+   $Date: $
+   $Revision: $
+   $Creator: Cao Khai(Casey Muratori's disciple) $
+   $Notice: (C) Copyright 2024 by Cao Khai, Inc. All Rights Reserved. $
+   ======================================================================== */
+#include "Tile.h"
+
+Tile* LoadTileMap();
+void drawTile(Map_content* content, uint8_t VaoID = 0, uint8_t shaderID = 0){
+    glUseProgram(shaderID);
+    glm::vec3 tempTilePos = glm::vec3(0.0f);
+    for (float y = 0.0; y < MapHeight; x+=TILE_HEIGHT){
+        for (float x = 0.0; x < MapWidth; x+=TILE_WIDTH){
+            glm::tempTilePos.x = x;
+            glm::tempTilePos.y = y;
+            model = glm::translate(model, tempTilePos);
+            setMat4(shaderID, "model", model);
+            glDrawArray(GL_TRIANGLES, 0, 36);
+        }        
+    }
+    // Repeatedly drawing a half cube as a tile
+}
+    
