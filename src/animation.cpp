ad19f0bed2762ff7c94cdd61cae009a8ed18b810
Cao Khai <goku1331999@gmail.com>
2025-12-16 11:50:54 +0700

    latest update

--- src/animation.cpp
+++ src/animation.cpp
@@ -7,4 +7,27 @@
    ======================================================================== */
 #include "animation.h"
 
+float GetScaleFactor(float animationTime, float lastkeyTime, float nextkeyTime){
+    float scaleFactor = 0.0f
+    float midWayLength = animationTime - lastkeyTime;
+    float timeDiff = nextkeyTime - lastkeyTime;
 
+    scaleFactor = midWayLength/timeDiff;
+
+    return scaleFactor;
+};
+
+glm::mat4 InterpolatePosition(Bone* bone = nullptr, float animationTime = 0.0f){
+    int pt0index = bone->m_Positions[bone->GetPositionIndex(animationTime)];
+    int pt1index = pt0index + 1;
+    float scaleFactor = GetScaleFactor(animationTime, bone->m_Positions[pt0index].timestamp, bone->m_Positions[pt1index].timestamp);
+
+glm:vec3 finalPosition = glm::mix(bone->m_Positions[pt0index].Position, bone->m_Positions[pt1index].Position, scaleFactor);
+
+    glm::mat4 finalTransformMatrix = glm::translate(glm::mat4(1.0f), finalPosition);
+
+    return finalTransformMatrix;
+};
+
+glm::mat4 InterpolateScaling(Bone* bone = nullptr, float animationTime = 0.0f);
+glm::mat4 InterpolateRotation(Bone* bone = nullptr, float animationTime = 0.0f);
