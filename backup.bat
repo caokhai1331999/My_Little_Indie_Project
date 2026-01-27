688b44543ea6c9d5f6b13e36f6649ea9be876311
Cao Khai <goku1331999@gmail.com>
2026-01-27 20:19:29 +0700

    ,,

--- backup.bat
+++ backup.bat
@@ -5,6 +5,6 @@ rem xcopy C:\"Users\klove\Downloads\External_Libraries" E:\"External_Libraries\"
 rem xcopy C:\"Users\klove\Documents\repos" E:\"repos\" /D /Y /S
 rem xcopy C:\Users\klove\AppData\Roaming\.ema* E:\"First Window app" /D /Y /S
 
-xcopy C:\Users\klove\AppData\Roaming\.ema* "W:\" /Y /S
+rem xcopy C:\Users\klove\AppData\Roaming\.ema* "W:\" /Y /S
 
 sh.exe .\backupOnline.sh
